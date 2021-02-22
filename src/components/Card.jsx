7b078baca2dd392c8c7b11e5999be4ecce117b8e
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-22 14:28:57 -0500

    Adding card

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../assets/styles/components/Card.module.css'
+
+const Card = () => {
+  return (
+    <div className="grid-flow-col">
+      <h1 className="text-red-600">holis</h1>
+    </div>
+  );
+}
+
+export default Card;
--- src/www/index.jsx
+++ src/www/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from '../components/Header'
+import Card from '../components/Card'
 
 const Container = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Card />
+    </div>
+
   )
 }
 
