3f7e77ab989adc7617f6360d554edf443d772165
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-23 17:48:27 -0500

    Add new template

--- templates/template-login/src/index.js
+++ templates/template-login/src/index.js
@@ -1,6 +1,36 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/* Template to login */
 
-console.log("this template is working")
+const container = document.querySelector('#app')
+container.className = 'h-screen  bg-no-repeat relative'
+
+const containerLogin = document.createElement('div')
+containerLogin.className = 'containerLogin bg-white w-2/4 absolute top-1/4 left-1/4 grid grid-cols-2 shadow-2xl'
+
+const bgCard = document.createElement('div')
+bgCard.className = 'bgCard text-center p-8'
+const containerForm = document.createElement('div')
+containerForm.className = 'text-center p-8'
+
+
+// IMG
+const img = document.createElement('img')
+img.src = '/form.png'
+bgCard.appendChild(img)
+
+// FORM
+const title = document.createElement('h1')
+title.innerText = 'Login your account'
+title.className = 'text-3xl text-indigo-500'
+
+const form = document.querySelector("#form")
+containerForm.appendChild(form)
+
+
+
+
+// CHILDREN
+container.appendChild(containerLogin)
+containerLogin.appendChild(bgCard)
+containerLogin.appendChild(containerForm)
+containerForm.appendChild(title)
+containerForm.appendChild(form)
