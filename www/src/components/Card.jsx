5548b850402c4c85420dfe16f79bc713178063f8
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-23 13:23:50 -0500

    useEffect

--- www/src/components/Card.jsx
+++ www/src/components/Card.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../assets/styles/components/Card.module.css'
 
+
 const Card = () => {
+  const [template, setTemplates] = React.useState([])
+
+  useEffect(() => {
+    fetch('https://doki-templates-default-rtdb.firebaseio.com/templates.json')
+      .then(response => response.json())
+      .then(data => setTemplates(data))
+  }, [])
+
   return (
-    <div className={styles.containerCard}>
-      <div className="bg-indigo-200">col 1</div>
-      <div className="bg-indigo-200">col 2</div>
-      <div className="bg-indigo-200">col 3</div>
+    <div>
+      <h1>Temp</h1>
+      <ul>
+        {
+          template.map(item => (
+            <li>{item.description}</li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
+
+
+
+
+/* return (
+  < div className="container-cards grid grid-cols-3 text-center m-32" >
+    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">
+      <h1>Title</h1>
+      <p>Description</p>
+      <p className="absolute inset-x-0 bottom-0 mb-4">Author</p>
+    </div>
+
+
+    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">col 2</div>
+    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">col 3</div>
+  </div >
+); */
+
+
 export default Card;
+
+
+
