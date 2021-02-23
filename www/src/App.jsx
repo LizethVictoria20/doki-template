15c6bcb74364f79329e3978fa2bcf25fde292312
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-23 08:17:39 -0500

    Add content and deleting cache

---
+++ www/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../src/components/Header'
+import Card from '../src/components/Card'
+
+function App() {
+  // Return the App component.
+  return (
+    <div className="App">
+      <Header />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
