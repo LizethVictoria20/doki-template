246483896cae02ae674bb4df866aaedb27572808
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-23 17:38:18 -0500

    Adding modal

--- www/src/components/Card.jsx
+++ www/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../../public/index.css';
 
 const Card = () => {
   const [template, setTemplates] = React.useState([])
@@ -22,14 +23,24 @@ const Card = () => {
     <div className="container-cards grid grid-cols-3 text-center m-32" >
       {
         template.map((item) =>
-          <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative" key={item.id}>
-            <h1>{item.name}</h1>
-            <p>{item.description}</p>
-            <p className="absolute inset-x-0 bottom-0 mb-4">{item.owner}</p>
-          </div>
+          <a href="#miModal" key={item.id}>
+            <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">
+              <h1>{item.name}</h1>
+              <p>{item.description}</p>
+              <p className="absolute inset-x-0 bottom-0 mb-4">{item.owner}</p>
+              <div id="miModal" className="modal bg-red-400  top-0 right-0 bottom-0 left-0">
+
+                <div className="modal-contenido bg-indigo-300 w-48 p-4 m-4 relative">
+                  <a href="#">X</a>
+                  <p>Content modal</p>
+                </div>
+              </div>
+            </div>
+          </a>
         )
       }
     </div>
+
   )
 }
 
