5a68b5bf2f9b112cdfe7dbfc8bb79b4dd8096afc
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-23 15:39:37 -0500

    rending info in cards

--- www/src/components/Card.jsx
+++ www/src/components/Card.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import styles from '../assets/styles/components/Card.module.css'
-
 
 const Card = () => {
   const [template, setTemplates] = React.useState([])
@@ -21,35 +19,19 @@ const Card = () => {
   }, [])
 
   return (
-    <div>
-      <h1>Temp</h1>
-      <ul>
-        {
-          template.map((item) =>
-            <li key={item.id}>{item.description}</li>
-          )
-        }
-      </ul>
+    <div className="container-cards grid grid-cols-3 text-center m-32" >
+      {
+        template.map((item) =>
+          <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative" key={item.id}>
+            <h1>{item.name}</h1>
+            <p>{item.description}</p>
+            <p className="absolute inset-x-0 bottom-0 mb-4">{item.owner}</p>
+          </div>
+        )
+      }
     </div>
   )
 }
-
-
-
-/* return (
-  < div className="container-cards grid grid-cols-3 text-center m-32" >
-    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">
-      <h1>Title</h1>
-      <p>Description</p>
-      <p className="absolute inset-x-0 bottom-0 mb-4">Author</p>
-    </div>
-
-
-    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">col 2</div>
-    <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">col 3</div>
-  </div >
-); */
-
 
 export default Card;
 
