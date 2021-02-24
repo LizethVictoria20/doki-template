43ec3e5065698e25a71ba65e6a31f21e0ae91676
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-24 09:52:33 -0500

    Add ID to modal

--- www/src/components/Card.jsx
+++ www/src/components/Card.jsx
@@ -23,16 +23,19 @@ const Card = () => {
     <div className="container-cards grid grid-cols-3 text-center m-32" >
       {
         template.map((item) =>
-          <a href="#miModal" key={item.id}>
+          <a href={`#miModal-${item.id}`} key={item.id}>
             <div className="h-60 shadow-2xl bg-indigo-200 rounded mx-4 m-20 p-6 relative">
               <h1>{item.name}</h1>
               <p>{item.description}</p>
+              <img src="" alt="" />
               <p className="absolute inset-x-0 bottom-0 mb-4">{item.owner}</p>
-              <div id="miModal" className="modal bg-red-400  top-0 right-0 bottom-0 left-0">
+              <div id={`miModal-${item.id}`} className="modal bg-red-400  top-0 right-0 bottom-0 left-0">
 
                 <div className="modal-contenido bg-indigo-300 w-48 p-4 m-4 relative">
                   <a href="#">X</a>
-                  <p>Content modal</p>
+                  <h1>{item.name}</h1>
+                  <p>Download</p>
+                  <p className="bg-gray-200">npx create-snowpack-app page-example --template @snowpack/template-login</p>
                 </div>
               </div>
             </div>
