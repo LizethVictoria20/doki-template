6b6958352c71c1c6fc9e2a842e0a6b2a0480c2d6
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-26 18:42:30 -0500

    img to render

--- templates/template-cards/src/index.js
+++ templates/template-cards/src/index.js
@@ -3,9 +3,22 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import confetti from 'canvas-confetti';
+const container = document.querySelector('#app')
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const title = document.createElement('h1')
+title.innerText = 'Pomme'
+title.className = 'text-center'
+
+const cardContainer = document.createElement('div')
+cardContainer.className = 'card-container grid grid-cols-3'
+
+fetch('/data.json')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.fruits.length; i++) {
+      console.log(data[i])
+    }
+  })
+
+container.appendChild(title)
+container.appendChild(cardContainer)
