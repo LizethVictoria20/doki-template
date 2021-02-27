edf71f0d5cebc6cedebb42a4ad33b603dbf13e3a
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-26 20:28:08 -0500

    adding information and styles

--- templates/template-cards/src/index.js
+++ templates/template-cards/src/index.js
@@ -10,14 +10,32 @@ title.innerText = 'Pomme'
 title.className = 'text-center'
 
 const cardContainer = document.createElement('div')
-cardContainer.className = 'card-container grid grid-cols-3'
+cardContainer.className = 'card-container grid grid-cols-3 '
 
 fetch('/data.json')
   .then(response => response.json())
   .then(data => {
-    for (let i = 0; i < data.fruits.length; i++) {
-      console.log(data[i])
-    }
+    const objFruits = Object.values(data)
+    objFruits.map((fruit) => {
+      for (let i = 0; i < fruit.length; i++) {
+        console.log(fruit[i])
+        const fruitContainer = document.createElement('div')
+        fruitContainer.className = 'fruitContainer shadow-2xl m-12 text-center h-40 w-52 relative p-6 rounded-lg bg-white'
+
+        const imgFruit = document.createElement('img')
+        imgFruit.src = fruit[i].img
+
+        const nameFruit = document.createElement('p')
+        nameFruit.innerText = fruit[i].name
+        nameFruit.className = 'absolute inset-x-0 bottom-0 mb-4'
+
+        cardContainer.appendChild(fruitContainer)
+        fruitContainer.appendChild(imgFruit)
+        fruitContainer.appendChild(nameFruit)
+      }
+
+    })
+
   })
 
 container.appendChild(title)
