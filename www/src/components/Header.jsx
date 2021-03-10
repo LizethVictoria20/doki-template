42de63336f59589dc589b5b0bf285906efc046f9
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-03-10 16:16:22 -0500

    adding template to header

--- www/src/components/Header.jsx
+++ www/src/components/Header.jsx
@@ -7,13 +7,15 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <nav className={styles.navbar}>
+      <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/documentation">Documentation</Link></li>
           <li><Link to="/tutorial">Tutorial</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/new-template">New Template</Link></li>
+          <li><Link to="/templates">Templates</Link></li>
+
         </ul>
       </nav>
     </div>
