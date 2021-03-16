320957aa400144c892aadc8bba3b85c316c0f8f0
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-03-15 20:28:31 -0500

    delete items from header

--- www/src/components/Header.jsx
+++ www/src/components/Header.jsx
@@ -12,8 +12,6 @@ const Header = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/documentation">Documentation</Link></li>
           <li><Link to="/tutorial">Tutorial</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/new-template">New Template</Link></li>
           <li><Link to="/templates">Templates</Link></li>
 
         </ul>
